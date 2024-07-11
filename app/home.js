1b949b8b8635124e6d5b945fd216133b7cdbd582
SUDAR2005 <sudarsettaiyan@gmail.com>
2024-07-11 10:03:41 +0530

    added footer and made pop up for google map

--- app/home.js
+++ app/home.js
@@ -1,35 +1,31 @@
-function myMenuFunction() {
-    var i = document.getElementById("navMenu");
-    if(i.className === "nav-menu") {
-        i.className += " responsive";
-    } else {
-        i.className = "nav-menu";
-    }
-}
-function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
-    var mainContent = document.querySelector('.main-content');
-    sidebar.classList.toggle('hidden');
-    mainContent.classList.toggle('full-width');
+document.addEventListener('DOMContentLoaded', (event) => {
+    const mainMap = L.map('map').setView([9.9197, 78.1194], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mainMap);
+    L.marker([9.9197, 78.1194]).addTo(mainMap)
+        .bindPopup('Madurai')
+        .openPopup();
+});
+
+function openMap() {
+    const popupOverlay = document.getElementById('mapPopup');
+    popupOverlay.style.display = 'flex';
+
+    // Initialize the popup map
+    const popupMap = L.map('popupMap').setView([9.9197, 78.1194], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(popupMap);
+
+    L.marker([9.9197, 78.1194]).addTo(popupMap)
+        .bindPopup('Madurai')
+        .openPopup();
 }
 
-var a = document.getElementById("loginBtn");
-var b = document.getElementById("registerBtn");
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-function login() {
-    x.style.left = "4px";
-    y.style.right = "-520px";
-    a.className += " white-btn";
-    b.className = "btn";
-    x.style.opacity = 1;
-    y.style.opacity = 0;
-}
-function register() {
-    x.style.left = "-510px";
-    y.style.right = "5px";
-    a.className = "btn";
-    b.className += " white-btn";
-    x.style.opacity = 0;
-    y.style.opacity = 1;
+function closeMap() {
+    const popupOverlay = document.getElementById('mapPopup');
+    popupOverlay.style.display = 'none';
 }
